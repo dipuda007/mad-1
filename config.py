8c8120f1167ebcef84f9b5bb0a6a25c901a68c5c
dipuda007 <24f2008594@ds.study.iitm.ac.in>
2026-01-02 07:04:22 +0530

    Fix database path for PythonAnywhere

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'placement-portal-secret-key-2026'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///placement_portal.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'placement_portal.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
